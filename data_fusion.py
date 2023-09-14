e2add2e2e1706900ea2b871a4775e540a031dfd2
sabernn <mnemat2@lsu.edu>
2023-09-14 12:09:24 -0500

    modified for different down samplings

--- data_fusion.py
+++ data_fusion.py
@@ -10,6 +10,8 @@ from itertools import cycle, islice
 import numpy as np
 import matplotlib.pyplot as plt
 
+plot = False
+
 data_dir = "G:\\Saber\\WAAM\\Kyungmin_May5\\NP files"
 data_dir = ""
 
@@ -39,31 +41,36 @@ print(Natten.shape)
 print(NDFI.shape)
 print(XA.shape)
 
-Natten_ds = Natten[0::10, 0::10, 0::10]
-NDFI_ds = NDFI[0::10, 0::10, 0::10]
-XA_ds = XA[0::10, 0::10, 0::10]
+# Natten_ds = Natten[0::5, 0::5, 0::5]
+# NDFI_ds = NDFI[0::5, 0::5, 0::5]
+# XA_ds = XA[0::5, 0::5, 0::5]
 
 
 
-print(Natten_ds.shape)
-print(NDFI_ds.shape)
-print(XA_ds.shape)
+# print(Natten_ds.shape)
+# print(NDFI_ds.shape)
+# print(XA_ds.shape)
 
-Natten_1D = Natten_ds.reshape(-1)
-NDFI_1D = NDFI_ds.reshape(-1)
-XA_1D = XA_ds.reshape(-1)
+# Natten_1D = Natten_ds.reshape(-1)
+# NDFI_1D = NDFI_ds.reshape(-1)
+# XA_1D = XA_ds.reshape(-1)
 
+Natten_1D = Natten.reshape(-1)
+NDFI_1D = NDFI.reshape(-1)
+XA_1D = XA.reshape(-1)
 
 
 AllIntensities = np.vstack((Natten_1D, NDFI_1D, XA_1D)).T
 
-# fig = plt.figure()
-# ax = fig.add_subplot(projection='3d')
-# plt.xlabel('Neutron Attenuation')
-# plt.ylabel('Neutron Dark Field Image')
-# plt.clabel('X-ray Attenuation')
-# plt.plot(Natten_1D[0::1000], NDFI_1D[0::1000], X_1D[0::1000],'.')
-# plt.show()
+
+# if plot:
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection='3d')
+    # plt.xlabel('Neutron Attenuation')
+    # plt.ylabel('Neutron Dark Field Image')
+    # plt.clabel('X-ray Attenuation')
+    # plt.plot(Natten_1D[0::1000], NDFI_1D[0::1000], X_1D[0::1000],'.')
+    # plt.show()
 
 # print(AllIntensities.shape)
 
@@ -73,19 +80,20 @@ AllIntensities = np.vstack((Natten_1D, NDFI_1D, XA_1D)).T
 slice_num = 200
 max_slice_num = Natten.shape[0]
 
-# fig = plt.figure()
-# # plt.size = (30, 90)
-# fig.add_subplot(1, 3, 1)
-# plt.imshow(Natten[slice_num])
-# plt.title(f'Neutron Attenuation, slice {slice_num}/{max_slice_num}')
-# fig.add_subplot(1, 3, 2)
-# plt.imshow(NDFI[slice_num])
-# plt.title(f'Neutron Dark Field Image, slice {slice_num}/{max_slice_num}')
-# fig.add_subplot(1, 3, 3)
-# plt.imshow(X[slice_num])
-# plt.title(f'X-ray Attenuation, slice {slice_num}/{max_slice_num}')
+# if plot:
+    # fig = plt.figure()
+    # # plt.size = (30, 90)
+    # fig.add_subplot(1, 3, 1)
+    # plt.imshow(Natten[slice_num])
+    # plt.title(f'Neutron Attenuation, slice {slice_num}/{max_slice_num}')
+    # fig.add_subplot(1, 3, 2)
+    # plt.imshow(NDFI[slice_num])
+    # plt.title(f'Neutron Dark Field Image, slice {slice_num}/{max_slice_num}')
+    # fig.add_subplot(1, 3, 3)
+    # plt.imshow(X[slice_num])
+    # plt.title(f'X-ray Attenuation, slice {slice_num}/{max_slice_num}')
 
-# plt.show()
+    # plt.show()
 
 
 # Clustering
@@ -179,85 +187,88 @@ np.save('y_pred.npy', y_pred)
 print(colors)
 colorsplot = ['c','b','g','r']
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.scatter3D(X[:, 0], X[:, 1], X[:,2], s=10, color=colors[y_pred])
-plt.xlabel('Neutron Attenuation')
-plt.ylabel('Neutron Dark Field Image')
-plt.clabel('X-ray Attenuation')
+if plot:
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter3D(X[:, 0], X[:, 1], X[:,2], s=10, color=colors[y_pred])
+    plt.xlabel('Neutron Attenuation')
+    plt.ylabel('Neutron Dark Field Image')
+    plt.clabel('X-ray Attenuation')
 
 
-# plt.stem(y_pred)
+    # plt.stem(y_pred)
 
-plt.show()
+    plt.show()
 
-fig = plt.figure()
-plt.scatter(X[:, 0], X[:, 2], s=10, color=colors[y_pred])
-xlabel = 'Neutron Attenuation'
-ylabel = 'X-ray Attenuation'
-# plt.legend(loc='upper right')
+    fig = plt.figure()
+    plt.scatter(X[:, 0], X[:, 2], s=10, color=colors[y_pred])
+    xlabel = 'Neutron Attenuation'
+    ylabel = 'X-ray Attenuation'
+    # plt.legend(loc='upper right')
 
-# plt.xlim(-2.5, 2.5)
-# plt.ylim(-2.5, 2.5)
-# plt.xticks(())
-# plt.yticks(())
-plt.text(
-            0.99,
-            0.01,
-            ("%.2fs" % (t1 - t0)).lstrip("0"),
-            transform=plt.gca().transAxes,
-            size=15,
-            horizontalalignment="right",
-        )
+    # plt.xlim(-2.5, 2.5)
+    # plt.ylim(-2.5, 2.5)
+    # plt.xticks(())
+    # plt.yticks(())
+    plt.text(
+                0.99,
+                0.01,
+                ("%.2fs" % (t1 - t0)).lstrip("0"),
+                transform=plt.gca().transAxes,
+                size=15,
+                horizontalalignment="right",
+            )
 
-plt.show()
+    plt.show()
 
 cluster1 = X[y_pred==0]
 cluster2 = X[y_pred==1]
 cluster3 = X[y_pred==2]
 cluster4 = X[y_pred==-1]
 
-fig = plt.figure()
-ax = fig.add_subplot(2,2,1,projection='3d')
-ax.scatter3D(cluster1[:, 0], cluster1[:, 1], cluster1[:,2], s=10, color=colors[0])
-ax.set_title('Cluster 1')
-ax.set_xlabel('NA (avg = ' + str(np.mean(cluster1[:,0])) + ')')
-ax.set_ylabel('NDFI (avg = ' + str(np.mean(cluster1[:,1])) + ')')
-ax.set_zlabel('XA (avg = ' + str(np.mean(cluster1[:,2])) + ')')
-ax.set_xlim(Natten_1D.min(), Natten_1D.max())
-ax.set_ylim(NDFI_1D.min(), NDFI_1D.max())
-ax.set_zlim(XA_1D.min(), XA_1D.max())
 
-ax = fig.add_subplot(2,2,2,projection='3d')
-ax.scatter3D(cluster2[:, 0], cluster2[:, 1], cluster2[:,2], s=10, color=colors[1])
-ax.set_title('Cluster 2')
-ax.set_xlabel('NA (avg = ' + str(np.mean(cluster2[:,0])) + ')')
-ax.set_ylabel('NDFI (avg = ' + str(np.mean(cluster2[:,1])) + ')')
-ax.set_zlabel('XA (avg = ' + str(np.mean(cluster2[:,2])) + ')')
-ax.set_xlim(Natten_1D.min(), Natten_1D.max())
-ax.set_ylim(NDFI_1D.min(), NDFI_1D.max())
-ax.set_zlim(XA_1D.min(), XA_1D.max())
+if plot:
+    fig = plt.figure()
+    ax = fig.add_subplot(2,2,1,projection='3d')
+    ax.scatter3D(cluster1[:, 0], cluster1[:, 1], cluster1[:,2], s=10, color=colors[0])
+    ax.set_title('Cluster 1')
+    ax.set_xlabel('NA (avg = ' + str(np.mean(cluster1[:,0])) + ')')
+    ax.set_ylabel('NDFI (avg = ' + str(np.mean(cluster1[:,1])) + ')')
+    ax.set_zlabel('XA (avg = ' + str(np.mean(cluster1[:,2])) + ')')
+    ax.set_xlim(Natten_1D.min(), Natten_1D.max())
+    ax.set_ylim(NDFI_1D.min(), NDFI_1D.max())
+    ax.set_zlim(XA_1D.min(), XA_1D.max())
 
-ax = fig.add_subplot(2,2,3,projection='3d')
-ax.scatter3D(cluster3[:, 0], cluster3[:, 1], cluster3[:,2], s=10, color=colors[2])
-ax.set_title('Cluster 3')
-ax.set_xlabel('NA (avg = ' + str(np.mean(cluster3[:,0])) + ')')
-ax.set_ylabel('NDFI (avg = ' + str(np.mean(cluster3[:,1])) + ')')
-ax.set_zlabel('XA (avg = ' + str(np.mean(cluster3[:,2])) + ')')
-ax.set_xlim(Natten_1D.min(), Natten_1D.max())
-ax.set_ylim(NDFI_1D.min(), NDFI_1D.max())
-ax.set_zlim(XA_1D.min(), XA_1D.max())
+    ax = fig.add_subplot(2,2,2,projection='3d')
+    ax.scatter3D(cluster2[:, 0], cluster2[:, 1], cluster2[:,2], s=10, color=colors[1])
+    ax.set_title('Cluster 2')
+    ax.set_xlabel('NA (avg = ' + str(np.mean(cluster2[:,0])) + ')')
+    ax.set_ylabel('NDFI (avg = ' + str(np.mean(cluster2[:,1])) + ')')
+    ax.set_zlabel('XA (avg = ' + str(np.mean(cluster2[:,2])) + ')')
+    ax.set_xlim(Natten_1D.min(), Natten_1D.max())
+    ax.set_ylim(NDFI_1D.min(), NDFI_1D.max())
+    ax.set_zlim(XA_1D.min(), XA_1D.max())
 
-ax = fig.add_subplot(2,2,4,projection='3d')
-ax.scatter3D(cluster4[:, 0], cluster4[:, 1], cluster4[:,2], s=10, color='red')
-ax.set_title('Cluster 4 (outliers)')
-ax.set_xlabel('NA (avg = ' + str(np.mean(cluster4[:,0])) + ')')
-ax.set_ylabel('NDFI (avg = ' + str(np.mean(cluster4[:,1])) + ')')
-ax.set_zlabel('XA (avg = ' + str(np.mean(cluster4[:,2])) + ')')
-ax.set_xlim(Natten_1D.min(), Natten_1D.max())
-ax.set_ylim(NDFI_1D.min(), NDFI_1D.max())
-ax.set_zlim(XA_1D.min(), XA_1D.max())
+    ax = fig.add_subplot(2,2,3,projection='3d')
+    ax.scatter3D(cluster3[:, 0], cluster3[:, 1], cluster3[:,2], s=10, color=colors[2])
+    ax.set_title('Cluster 3')
+    ax.set_xlabel('NA (avg = ' + str(np.mean(cluster3[:,0])) + ')')
+    ax.set_ylabel('NDFI (avg = ' + str(np.mean(cluster3[:,1])) + ')')
+    ax.set_zlabel('XA (avg = ' + str(np.mean(cluster3[:,2])) + ')')
+    ax.set_xlim(Natten_1D.min(), Natten_1D.max())
+    ax.set_ylim(NDFI_1D.min(), NDFI_1D.max())
+    ax.set_zlim(XA_1D.min(), XA_1D.max())
+
+    ax = fig.add_subplot(2,2,4,projection='3d')
+    ax.scatter3D(cluster4[:, 0], cluster4[:, 1], cluster4[:,2], s=10, color='red')
+    ax.set_title('Cluster 4 (outliers)')
+    ax.set_xlabel('NA (avg = ' + str(np.mean(cluster4[:,0])) + ')')
+    ax.set_ylabel('NDFI (avg = ' + str(np.mean(cluster4[:,1])) + ')')
+    ax.set_zlabel('XA (avg = ' + str(np.mean(cluster4[:,2])) + ')')
+    ax.set_xlim(Natten_1D.min(), Natten_1D.max())
+    ax.set_ylim(NDFI_1D.min(), NDFI_1D.max())
+    ax.set_zlim(XA_1D.min(), XA_1D.max())
 
 
 
-plt.show()
+    plt.show()
