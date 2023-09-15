d5457f4c0004c515c29bbc1731bba94a83ed930e
sabernn <mnemat2@lsu.edu>
2023-09-15 17:18:17 -0500

    plotting 1D histograms

--- clustered_intensity.py
+++ clustered_intensity.py
@@ -1,6 +1,7 @@
 
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 vec_clust = np.load('y_pred_x5_HDBSCAN.npy')
 # vol_clust = vec_clust.reshape(80,46,46)
@@ -36,5 +37,53 @@ Natten_al = Natten_x5_1D[al_ind]
 NDFI_al = NDFI_x5_1D[al_ind]
 XA_al = XA_x5_1D[al_ind]
 
+fig = plt.figure()
+plt.title('1D Histograms')
+fig.add_subplot(311)
+# plt.scatter(Natten_ox, NDFI_ox, c='r', s=1)
+# plt.scatter(Natten_vc, NDFI_vc, c='b', s=1)
+# plt.scatter(Natten_bg, NDFI_bg, c='g', s=1)
+# plt.scatter(Natten_al, NDFI_al, c='k', s=1)
+# plt.hist(Natten_vc, bins=100)
+plt.hist(Natten_bg, bins=100)
+plt.hist(Natten_al, bins=100)
+plt.hist(Natten_ox, bins=100, color='r')
+
+# plt.title('1D Histogram of Natten')
+plt.xlabel('Natten')
+plt.ylabel('Count')	
+
+plt.legend(['Background', 'Aluminum', 'Oxide'])
+
+
+# plt.figure()
+fig.add_subplot(312)
+
+plt.hist(NDFI_bg, bins=100)
+plt.hist(NDFI_al, bins=100)
+plt.hist(NDFI_ox, bins=100, color='r')
+
+# plt.title('1D Histogram of NDFI')
+plt.xlabel('NDFI')
+plt.ylabel('Count')
+
+plt.legend(['Background', 'Aluminum', 'Oxide'])
+# plt.show()
+
+
+# plt.figure()
+fig.add_subplot(313)
+
+plt.hist(XA_bg, bins=100)
+plt.hist(XA_al, bins=100)
+plt.hist(XA_ox, bins=100, color='r')
+
+# plt.title('1D Histogram of XA')
+plt.xlabel('XA')
+plt.ylabel('Count')
+
+plt.legend(['Background', 'Aluminum', 'Oxide'])
+
+plt.show()
 
 
